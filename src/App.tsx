0da6c53f0924761769315f250f93440758c0ccd6
Sidnei <sidneisonic@hotmail.com>
2024-03-15 11:27:34 -0300

    aula 59 conhecendo useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
-import { useId } from "react";
+import { useId, useRef } from "react";
 
 const App = () => {
   const inputID = useId();
 
+  // const handleClick = () => {
+  //   const inputElement: HTMLInputElement | null = document.querySelector("#input");
+  //   if (inputElement) {
+  //     inputElement.focus();
+  //   }
+
+  const inputElement = useRef<HTMLInputElement>(null);
+  const handleClick = () => {
+    const current = inputElement.current;
+    if (!current) {
+      return;
+    }
+    
+    if (inputElement.current) {
+      inputElement.current.focus();
+    }
+  }
+
   return (
-    <div>
-      <label htmlFor={inputID}>Insira algo</label>
-      <input type="text" placeholder="Digite alog" id={inputID}/>
-    </div>
+    <>
+      <div>
+        <label htmlFor={inputID}>Insira algo</label>
+        <input type="text" placeholder="Digite alog" id={inputID}/>
+      </div>
+      <div>
+        <input type="text" name="" id="input" ref={inputElement} />
+        <button onClick={handleClick}>Focar no input</button>
+      </div>
+    </>
   );
 }
 
